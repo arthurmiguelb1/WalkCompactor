5cd33f0423dbca034dd00a876e364ced90f109e0
Wesley <wesleydos504@gmail.com>
2019-02-27 00:57:28 -0300

    definição do tipo h_tree

--- hufman_tree.c
+++ hufman_tree.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "libs/heap.h"
+
+typedef unsigned char UCHAR;
+
+typedef struct h_tree{
+	UCHAR simbol;
+	int frequency;
+	struct h_tree *left;
+	struct h_tree *right;
+} No;
+
+typedef No* NO;
+
+NO create_no(){
+	NO htree = (NO) malloc(sizeof(No));
+	htree->left = NULL;
+	htree->right = NULL;
+	int frequency = 0;
+	return htree;
+}
+
+
+void** create_htree_array(int i){
+	void** array = (void**) malloc(sizeof(void*));
+	return array;
+}
+
+
+short compare_nos(void* m, void* n){
+	NO a, b;
+	a = (NO) m;
+	b = (NO) n;
+	if (a->frequency < b->frequency) return 1;
+	return 0;
+}
 
 
 int main(){
